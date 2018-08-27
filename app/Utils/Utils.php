33879b91e477eaad2d3ee626e7b6d09691916f36
vietlubu <baoviet3010@gmail.com>
2018-08-27 15:26:52 +0700

    Add helpers function

---
+++ app/Utils/Utils.php
@@ -0,0 +1,35 @@
+<?php
+
+if (!function_exists('dd')) {
+    /**
+     * Debug helper function
+     *
+     * @param string $data data
+     *
+     * @return void
+     */
+    function dd($data = null)
+    {
+        var_dump($data);
+        die;
+    }
+}
+
+if (!function_exists('view')) {
+    /**
+     * View Helper function
+     *
+     * @param string $view   view path
+     * @param array  $params list param
+     *
+     * @return void
+     */
+    function view($view, array $params = [])
+    {
+        $view = str_replace('.', '/', $view);
+        ob_start();
+        extract($params, EXTR_SKIP);
+        require_once APP_PATH . "Views/{$view}.php";
+        ob_end_flush();
+    }
+}
