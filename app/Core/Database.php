db39462b290b97371af1dd0d50c9201ea9340fa0
vietlubu <baoviet3010@gmail.com>
2018-08-28 10:28:11 +0700

    Add function create database

--- app/Core/Database.php
+++ app/Core/Database.php
@@ -14,8 +14,30 @@ class Database extends PDO
         try {
             $dbPatch = APP_ROOT . "database/database.db";
             parent::__construct("sqlite:" . $dbPatch);
+
+            $this->createTasksTable();
         } catch (\Exception $e) {
             error_log("Could not connect to database: " . $e->getMessage());
         }
+    }
+
+    /**
+     * Create table `tasks`
+     *
+     * @return void
+     */
+    private function createTasksTable()
+    {
+        $sqlCreateTable = "
+            CREATE TABLE IF NOT EXISTS `tasks` (
+                `id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                `name`	TEXT NOT NULL,
+                `starting_date`	TEXT NOT NULL,
+                `ending_date`	TEXT,
+                `status`	INTEGER NOT NULL DEFAULT 1
+            );
+        ";
+
+        $this->exec($sqlCreateTable);
     }
 }
